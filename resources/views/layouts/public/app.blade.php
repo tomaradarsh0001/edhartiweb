ae3bc1db5863686016e23d28e0148856a537adad
Adarsh <adarshtomar@gmail.com>
2025-02-11 18:14:02 +0530

    updated jquery

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -297,7 +297,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <!-- External JS libraries -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> --}}
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/frontend/assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/frontend/assets/js/bootstrap.bundle.min.js') }}"></script>
 
